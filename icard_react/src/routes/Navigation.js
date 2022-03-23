8b2aedbb53fc063f9cc528b2575214e91e7b08b8
MessDeveloper <enric.martinezromero@gmail.com>
2022-03-23 01:12:19 +0100

    Initial commit

---
+++ icard_react/src/routes/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+
+export function Navigation() {
+  return (
+
+    <BrowserRouter>
+      <Routes>
+       <Route>Navigation...</Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
